9ae12aabcb159f62f49139e9764e0270dfb6912f
anthonyngo <mail.anthonyngo@gmail.com>
2024-09-06 20:27:34 +1000

    Set required inputs.

--- src/gan.py
+++ src/gan.py
@@ -22,3 +22,41 @@ print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 torch.use_deterministic_algorithms(True) # Needed for reproducible results
+
+# Root directory for dataset
+dataroot = "/home/groups/comp3710/"
+
+# Number of workers for dataloader
+workers = 2
+
+# Batch size during training
+batch_size = 128
+
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+image_size = 64
+
+# Number of channels in the training images. For bnw images we set to 1
+nc = 1
+
+# Size of z latent vector (i.e. size of generator input)
+# it is sampled from a standard normal distribution
+nz = 100
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discriminator
+ndf = 64
+
+# Number of training epochs
+num_epochs = 5
+
+# Learning rate for optimizers
+lr = 0.0002
+
+# Beta1 hyperparameter for Adam optimizers
+beta1 = 0.5
+
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = 1
